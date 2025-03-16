6d51070c31570e9af7ea28e6ae06ea45be588964
AnHawk <104738714+AnHawk@users.noreply.github.com>
2025-03-16 21:23:28 +0200

    adaptiv

--- app.js
+++ app.js
@@ -92,3 +92,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
+
+document.addEventListener('DOMContentLoaded', function () {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav');
+
+    burger.addEventListener('click', function () {
+        nav.classList.toggle('active');
+        burger.classList.toggle('active');
+    });
+});
