70b94c5833c45223a417e46e7b795b6c033ecbe1
AnHawk <104738714+AnHawk@users.noreply.github.com>
2025-05-23 10:18:44 +0300

    d

--- courses.js
+++ courses.js
@@ -142,6 +142,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 if (data.ok) {
                     console.log("Ваша заявка отправлена!");
                     modal.style.display = "none";
+                    window.location.href = "thank-you.html";
                 } else {
                     console.log("Ошибка при отправке. Попробуйте еще раз.");
                 }
